e80debfd66d7774745392ca5eb3105824f081386
Brandon <brandoncyu@gmail.com>
2018-06-26 13:08:12 -0700

    added code

--- main.js
+++ main.js
@@ -1,9 +1,59 @@
 axios.get('http://localhost:3000/cats')
   .then(result => {
     const cats = result.data.data
-    const items = cats.reduce((acc, cat) => `<li>${cat.name}</li>`, '')
-    document.querySelector('ul').innerHTML = items
+    console.log(cats)
+    const string = ''
+    let holder = document.querySelector('ul')
+    cats.forEach(cat => {
+      let lineItem = document.createElement('li')
+      lineItem.innerHTML = cat.name
+      holder.appendChild(lineItem)
+    })
   })
   .catch(err => {
     console.log('Do not worry, everything is fine.')
   })
+
+
+
+createButton = document.getElementById('posts-create')
+//Add an event listener to the create new cat button
+createButton.addEventListener('click', createForm)
+
+
+function createForm(){
+  //On click, show a form on the page to create a new cat
+  newForm = document.createElement('form')
+  newForm.setAttribute('id', 'cat-form')
+  newForm.innerHTML = `<label for = "cat-name">Cat Name</label><br><input type = "text" name="cat-name" id="cat-name" placeholder="what is your cat's name?"><br><input type = 'submit' class="btn btn-primary">`
+
+  let formContainer = document.getElementById('form-container')
+  formContainer.appendChild(newForm)
+
+  buttonContainer = document.getElementById('button-container')
+  buttonContainer.removeChild(createButton)
+
+
+  newForm = document.getElementById('cat-form')
+  newForm.addEventListener('submit', function(event){
+    event.preventDefault()
+    let catName = document.getElementById('cat-name')
+
+    //On form submit, post a new cat to our Cats API
+    axios.post('http://localhost:3000/cats', {name: catName.value})
+      .then(result => {
+        const newCat = result.data.data
+        let items = document.createElement('li')
+        //On form submit, add to the list of cats on the page
+        items.innerHTML = newCat.name
+        document.querySelector('ul').appendChild(items)
+      })
+
+    //On form submit, hide the form and clear the input
+    formContainer.removeChild(newForm)
+    buttonContainer.innerHTML = `<button id="posts-create" class="btn btn-primary">Create a New Kitten</button>`
+
+    createButton = document.getElementById('posts-create')
+    createButton.addEventListener('click', createForm)
+  })
+}
--- readme.md
+++ readme.md
@@ -6,11 +6,11 @@ Example repository for using axios.
 
 - [x] Add a hard-coded list for all our cats
 - [x] Retrieve all our cats from our Cats API
-- [ ] Add an event listener to the create new cat button
-- [ ] On click, show a form on the page to create a new cat
-- [ ] On form submit, hide the form and clear the input
-- [ ] On form submit, post a new cat to our Cats API
-- [ ] On form submit, add to the list of cats on the page
+- [x] Add an event listener to the create new cat button
+- [x] On click, show a form on the page to create a new cat
+- [x] On form submit, hide the form and clear the input
+- [x] On form submit, post a new cat to our Cats API
+- [x] On form submit, add to the list of cats on the page
 
 ### Stretch
 
